1de723090769a32f0d9a37029edaea3bc176fc1d
oscaroceguerabibriesca <oscaroceguera@gmail.com>
2017-02-01 15:09:30 -0700

    pug using arrow function

--- index.js
+++ index.js
@@ -8,25 +8,32 @@ var app = express()
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'pug')
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'Express con pug',
     message: 'Hola estamos en el home'
   })
 })
 
-app.get('/panel', function(req, res){
+app.get('/nombre/:myName', (req, res) => {
+  res.render('index', {
+    title: 'Express con pug',
+    message: `Hola ${req.params.myName}`
+  })
+})
+
+app.get('/panel', (req, res) => {
   res.render('index', {
     title: 'Express con pug',
     message: 'Hola en el dashboard'
   })
 })
 
-app.use(function(req, res, next){
+app.use( (req, res, next) => {
   res.status(404).send("Sorry taht route does not exist")
 })
 
 
-app.listen(3000, function (){
+app.listen(3000, () => {
   console.log('Server starts: 3000');
 })
